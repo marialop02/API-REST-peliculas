1477f36d097d9ef8dc8301be54b8fe61d5f8a6ba
marialop02 <lucia.lopez@est.iudigital.edu.co>
2026-03-08 21:54:57 -0500

    establecer endpoints para todos los módulos y puntos de entrada en index.js, configurar rutas del API (director.js, media.js, productora.js, tipo.js)

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 app.use('/api/genero', require('./routes/genero'));
+app.use('/api/director', require('./routes/director'));
+app.use('/api/productora', require('./routes/productora'));
+app.use('/api/tipo', require('./routes/tipo'));
+app.use('/api/media', require('./routes/media'));
 
 getConnection();
 
